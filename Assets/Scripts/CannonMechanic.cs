5c959c34f002a1a669152f49e4b384920e32e458
NickKRay1234 <ul0250896@edu.uni.lodz.pl>
2022-03-31 21:02:40 +0200

    Added CannonMechanic.cs

---
+++ Assets/Scripts/CannonMechanic.cs
@@ -0,0 +1,41 @@
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class CannonMechanic : MonoBehaviour
+{
+    [SerializeField] private float _power = 2f;
+    [SerializeField] private int _amountOfDots = 15;
+
+    private Vector2 _startPos;
+    private bool _isShooting;
+    private bool _isAiming;
+
+    private GameObject Dots;
+    private List<GameObject> _projectilesPath;
+
+    private Rigidbody2D _ballBody;
+    public GameObject ballPrefab;
+    public GameObject ballsContainer;
+    
+    private void Start()
+    {
+        Dots = GameObject.Find("dots");
+        _projectilesPath = Dots.transform.Cast<Transform>().ToList().ConvertAll(t => t.gameObject);
+        for (int i = 0; i < _projectilesPath.Count; i++)
+        {
+            _projectilesPath[i].GetComponent<Renderer>().enabled = false;
+        }
+    }
+
+    private void Aim()
+    {
+        
+    }
+
+    
+    private void Update()
+    {
+        
+    }
+}
