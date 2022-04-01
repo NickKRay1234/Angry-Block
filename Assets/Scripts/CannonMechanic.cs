76be580744fcebc11b0a6a7e859763f8e8521b83
NickKRay1234 <ul0250896@edu.uni.lodz.pl>
2022-04-01 23:01:44 +0200

    Added new methods in script CannonMechanic.cs:  - Path Calculation;  - ShootForce;  - DotPath;  - Aim is updated;

--- Assets/Scripts/CannonMechanic.cs
+++ Assets/Scripts/CannonMechanic.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
@@ -20,7 +21,7 @@ public class CannonMechanic : MonoBehaviour
     
     private void Start()
     {
-        Dots = GameObject.Find("dots");
+        Dots = GameObject.Find("Dots");
         _projectilesPath = Dots.transform.Cast<Transform>().ToList().ConvertAll(t => t.gameObject);
         for (int i = 0; i < _projectilesPath.Count; i++)
         {
@@ -31,27 +32,61 @@ public class CannonMechanic : MonoBehaviour
     private void Aim()
     {
         if (_isShooting) return;
-        if (Input.GetAxis("Fire1") == 1)
+        if (Input.GetMouseButtonDown(0))
         {
             if (!_isAiming)
             {
-                print("1St part");
-                //cal
+                _isAiming = true;
+                _startPos = Input.mousePosition;
             }
             else
             {
                 //Aim Cal path
+                PathCalculation();
             }
         }
-        else
+        else if(_isAiming && !_isShooting)
         {
+            _isAiming = false;
+            print("Shoot");
             //Shoot
         }
+    }
+
+    private Vector2 ShootForce(Vector3 force)
+    {
+        return (new Vector2(_startPos.x, _startPos.y) - new Vector2(force.x, force.y)) * _power;
+    }
+
+    private Vector2 DotPath(Vector2 startPosition, Vector2 startVelocity, float t)
+    {
+        return startPosition + startVelocity * t + 0.5f * Physics2D.gravity * t * t;
+    }
+
+    private void PathCalculation()
+    {
+        Vector2 vel = ShootForce(Input.mousePosition) * Time.fixedDeltaTime / _ballBody.mass;
+
+        for (int i = 0; i < _projectilesPath.Count; i++)
+        {
+            _projectilesPath[i].GetComponent<Renderer>().enabled = true;
+            float t = i / 15f;
+            Vector3 point = DotPath(transform.position, vel, t);
+            point.z = 1;
+            _projectilesPath[i].transform.position = point;
+        }
+        
     }
 
     
     private void Update()
     {
+        _ballBody = ballPrefab.GetComponent<Rigidbody2D>();
         Aim();
+        /*if (GC.shotCount <= 3 && !IsMouseOverUI())
+        {
+            Aim();
+            Rotate();
+        }*/
     }
 }
